e2791ff0826fdfd3a5332caaf80697a0048993d3
Ben Osborne <osborne.ben@gmail.com>
2021-11-18 12:05:01 -0600

    fixed appearance of price range and additional info fields

--- NamesOutOfAHat2.Model/Hat.cs
+++ NamesOutOfAHat2.Model/Hat.cs
@@ -4,10 +4,13 @@ namespace NamesOutOfAHat2.Model
 {
     public class Hat
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = default!;
+
+        [MaxLength(10000)]
+        public string AdditionalInformation { get; set; } = default!;
 
         [MaxLength(255)]
-        public string PriceRange { get; set; }
+        public string PriceRange { get; set; } = default!;
 
         public Participant? Organizer { get; set;  }
 
