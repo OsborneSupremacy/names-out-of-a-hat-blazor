761b34186d664a7579d3227df025ce6f0da87b5a
Ben Osborne <osborne.ben@gmail.com>
2021-11-17 18:08:55 -0600

    Did some work on staging mode

---
+++ NamesOutOfAHat2.Utility/GenericExtensions.cs
@@ -0,0 +1,9 @@
+﻿
+namespace NamesOutOfAHat2.Utility
+{
+    public static class GenericExtensions
+    {
+        public static bool In<T>(this T input, params T[] values) => 
+            values.Contains(input);
+    }
+}
