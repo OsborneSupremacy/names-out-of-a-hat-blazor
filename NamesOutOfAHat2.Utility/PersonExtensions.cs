f5676e29e68dcdb4becffe797199f9994abf684d
Ben Osborne <osborne.ben@gmail.com>
2021-11-20 16:12:32 -0600

    added PersonExtensions

---
+++ NamesOutOfAHat2.Utility/PersonExtensions.cs
@@ -0,0 +1,11 @@
+﻿using NamesOutOfAHat2.Model;
+
+namespace NamesOutOfAHat2.Utility
+{
+    public static class PersonExtensions
+    {
+        public static string WriteDisplayName(this Person input) =>
+           !string.IsNullOrWhiteSpace(input?.Name ?? string.Empty) ? input!.Name : "Participant";
+
+    }
+}
