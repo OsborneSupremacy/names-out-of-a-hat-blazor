c21666a47d02e22ce13d28f96c24765255ef9087
Ben Osborne <osborne.ben@gmail.com>
2021-11-20 07:31:51 -0600

    added HatService

---
+++ NamesOutOfAHat2.Service/HatService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NamesOutOfAHat2.Model;
+using NamesOutOfAHat2.Utility;
+
+namespace NamesOutOfAHat2.Service
+{
+    [ServiceLifetime(ServiceLifetime.Scoped)]
+    public class HatService
+    {
+        public Hat AddParticipant(Hat hat, Person person)
+        {
+            hat.Participants ??= new List<Participant>();
+
+            var newGiverRecipients = new List<Recipient>();
+
+            // make person a recipient for all existing participants
+            foreach (Participant ep in hat.Participants)
+            {
+                // new person is recipient for existing participant
+                ep.Recipients.Add(new Recipient(person, true));
+                // existing participant is recipient for new person
+                newGiverRecipients.Add(new Recipient(ep.Person, true));
+            }
+
+            // add new participant to hat
+            var participant = new Participant(person)
+            {
+                Recipients = newGiverRecipients
+            };
+
+            hat.Participants.Add(participant);
+
+            return hat;
+        }
+    }
+}
