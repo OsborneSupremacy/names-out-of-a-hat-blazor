3df47d4e2a6861a0b6dbbab28d012d9ec451000a
Ben Osborne <ben@osbornesupremacy.com>
2025-11-08 16:20:07 -0600

    Add AWS data sources and lambda permission for API Gateway invocation

--- iac/terraform/modules/lambda/exec-role.tf
+++ iac/terraform/modules/lambda/exec-role.tf
@@ -19,3 +19,11 @@ resource "aws_iam_role_policy_attachment" "exec-role-attachment-lambda-basic-exe
   role       = aws_iam_role.exec-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_lambda_permission" "allow_apigw_invoke" {
+  statement_id  = "AllowExecutionFromAPIGateway-${var.gateway_http_operation_name}"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${var.gateway_rest_api_id}/*/*/*"
+}
