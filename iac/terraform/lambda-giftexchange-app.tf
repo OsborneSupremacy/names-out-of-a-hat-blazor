eddd5b32a09f6a33badc271356fcc1f0c7606f79
Ben Osborne <ben@osbornesupremacy.com>
2025-12-28 16:27:26 -0600

    Fix source ARN format for API Gateway Lambda invocation permission

--- iac/terraform/lambda-giftexchange-app.tf
+++ iac/terraform/lambda-giftexchange-app.tf
@@ -41,7 +41,7 @@ resource "aws_lambda_permission" "giftexchange_app_allow_apigw_invoke" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.giftexchange_app.arn
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.giftexchange-gateway.id}/*/*/*"
+  source_arn    = "arn:aws:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.giftexchange-gateway.id}/*/*"
 }
 
 resource "aws_iam_role_policy" "giftexchange_app_dynamodb_policy" {
