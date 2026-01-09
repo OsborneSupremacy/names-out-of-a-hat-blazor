974f17dcdc8aca9e3b66ff05092a1f8cd374caa8
Ben Osborne <ben@osbornesupremacy.com>
2026-01-09 14:24:31 -0600

    Add organizer verification endpoint and request schema

--- iac/terraform/api-gateway-resource-organizer.tf
+++ iac/terraform/api-gateway-resource-organizer.tf
@@ -10,6 +10,12 @@ resource "aws_api_gateway_resource" "organizer-verification-resource" {
   path_part   = "verification"
 }
 
+resource "aws_api_gateway_resource" "organizer-verify-resource" {
+  rest_api_id = aws_api_gateway_rest_api.giftexchange-gateway.id
+  parent_id   = aws_api_gateway_resource.organizer-verification-resource.id
+  path_part   = "verify"
+}
+
 module "gateway-options-response-organizer" {
   source              = "./modules/gateway-options-response"
   gateway_rest_api_id = aws_api_gateway_rest_api.giftexchange-gateway.id
---
+++ iac/terraform/api-verify-organizer.tf
@@ -0,0 +1,18 @@
+module "lambda-verify-organizer" {
+  source                                            = "./modules/api"
+  gateway_rest_api_id                               = aws_api_gateway_rest_api.giftexchange-gateway.id
+  gateway_resource_id                               = aws_api_gateway_resource.organizer-verify-resource.id
+  gateway_http_method                               = "POST"
+  gateway_http_operation_name                       = "VerifyOrganizer"
+  gateway_method_request_parameters                 = {}
+  gateway_method_request_model_name                 = "VerifyOrganizerRequest"
+  gateway_method_request_model_description          = "A request to verify an organizer with a verification code."
+  gateway_method_request_model_schema_file_location = "../../src/GiftExchange.Library/Schemas/VerifyOrganizerRequest.schema.json"
+  include_404_response                              = true
+  include_409_response                              = false
+  good_response_model_name                          = ""
+  good_response_model_description                   = ""
+  good_response_model_schema_file_location          = ""
+  api_name                                          = "giftexchange-verify-organizer"
+  lambda_invoke_arn                                 = aws_lambda_function.giftexchange_app.invoke_arn
+}
