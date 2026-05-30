af1ce8c68ae6ac633e31ebf2435e06ec2dc48d6e
Ben Osborne <ben@osbornesupremacy.com>
2026-05-29 22:49:14 -0500

    fix: move AWS-managed Web ACL configuration from waf.tf to cloudfront-frontend.tf for better organization

--- iac/terraform/waf.tf
+++
@@ -1,7 +0,0 @@
-# AWS-managed Web ACL created automatically by CloudFront free tier
-# This Web ACL is created and managed by AWS CloudFront
-data "aws_wafv2_web_acl" "cloudfront_managed" {
-  name  = "CreatedByCloudFront-6bc475d2"
-  scope = "CLOUDFRONT"
-}
-
